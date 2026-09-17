1aa2477b5531fea1ac21090125e4c38391d20914
root <josueomejiap@gmail.com>
2026-09-17 01:30:31 +0000

    clase 160926

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,39 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('app-empleados');
+  titulo = "Listado de empleados";
+  cuadroNombre: string = "";
+  cuadroApellido: string = "";
+  cuadroCargo: string = "";
+  cuadroSalario: number = 0;
+  empleados: Empleado[] = [
+    new Empleado("Josue", "Mejia", "Presidente", 5000),
+    new Empleado("Esdras", "Palacios", "Gerente", 4000),
+    new Empleado("Jorge", "Menjivar", "Supervisor", 3000),
+    new Empleado("Javier", "Contreras", "Empleado", 2000),
+  ];
+
+  agregar_empleado() {
+    let mi_empleado = new Empleado(
+      this.cuadroNombre,
+      this.cuadroApellido,
+      this.cuadroCargo,
+      this.cuadroSalario
+    );
+    this.empleados.push(mi_empleado);
+    // Limpiar los campos del formulario
+    this.cuadroNombre = "";
+    this.cuadroApellido = "";
+    this.cuadroCargo = "";
+    this.cuadroSalario = 0;
+  }
 }
